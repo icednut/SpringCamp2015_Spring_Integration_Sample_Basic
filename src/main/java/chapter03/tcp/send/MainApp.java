f439dc6eb98d0dc94678a6a164189c324a04a266
1002371 <1002371@SKP1002371MN001.local>
2015-04-17 15:18:50 +0900

    프로젝트 초기화

--- src/main/java/chapter03/tcp/send/MainApp.java
+++ src/main/java/chapter03/tcp/send/MainApp.java
@@ -1,15 +1,9 @@
 package chapter03.tcp.send;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * @author wglee21g@gmail.com
  */
 public class MainApp {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/chapter03/tcp-send-context.xml");
-		TcpSendGateway sendGateway = context.getBean("sendGateway", TcpSendGateway.class);
-
-		sendGateway.send("Hello World!");
 	}
 }
