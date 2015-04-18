26c8dda5dcd6d645644f4c83b474e48f0d915b0f
1002371 <1002371@SKP1002371MN001.local>
2015-04-18 12:20:08 +0900

    tcp 메시지 송수신 Main 개발

--- src/main/java/chapter03/tcp/send/MainApp.java
+++ src/main/java/chapter03/tcp/send/MainApp.java
@@ -1,9 +1,15 @@
 package chapter03.tcp.send;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author wglee21g@gmail.com
  */
 public class MainApp {
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/chapter03/tcp-send-context.xml");
+		TcpSendGateway tcpSendGateway = context.getBean("tcpSendGateway", TcpSendGateway.class);
+
+		tcpSendGateway.sendMessage("Hello World!!!!!!!!!!!!");
 	}
 }
